51f9b7db9bb1cec0125760f624919f763c4947c6
Nikos <nikos.gorogiannis@gmail.com>
2014-03-18 20:56:21 +0000

    Yet another update to the build system: now spot libraries are statically linked.

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -1,6 +1,8 @@
+(* I HATE OCAMLBUILD !!!!! *)
+
 open Ocamlbuild_plugin ;;
 
-(* change these to match you system *)
+(* change these to match your system *)
 
 (* where standard .h headers for ocaml are installed *)
 (* on Debian/Ubuntu these are in package ocaml-nox *)
@@ -13,6 +15,8 @@ let ocaml_melt_lib_path = "/usr/lib/ocaml/melt"
 (* base path for spot library *) 
 let spot_path = "/usr/local"
 
+(* these have to be manually specified as ocamlbuild will not automatically *)
+(* recognise them *)
 let headers = 
   [ 
     "src/soundness/proof_aut.hpp"; 
@@ -20,11 +24,12 @@ let headers =
     "src/soundness/trace.hpp" 
   ] 
 
+(* you shouldn't normally have to change anything below this point *)
 let spot_include_path = spot_path ^ "/include/spot"
-let spot_lib_path = spot_path ^ "/lib/spot"
 
 let _ = dispatch begin function
   | After_rules ->
+    (* declare melt as external OCaml library *)
     ocaml_lib ~extern:true ~dir:ocaml_melt_lib_path "latex";
 
     (* how to compile "c" files, really C++ *)
@@ -36,22 +41,37 @@ let _ = dispatch begin function
         A"-ccopt"; A("-I" ^ spot_include_path)]);
     
     
-    (* how to link everything together *)
-    flag ["link"; "ocaml"; "use_spot"]
-          (S[
-            A"-ccopt"; A("-L" ^ spot_lib_path); 
-            (* following option is a workaround for a bug in ocaml/ocamlbuild 3.x *)
-            (* that reorders linker arguments *)
-            A"-cclib"; A"-Wl,--no-as-needed";
-            A"-cclib"; A"-lbdd";
-            A"-cclib"; A"-lspot";
-            A"-cclib"; A"-lstdc++"
-            ]);
-
-    (* the path to the .a file is necessary *)
-    dep ["link"; "ocaml"; "use_libsoundness"] ["src/soundness/libsoundness.a"];
+    (* how to link everything together that uses libsoundness *)
     flag ["link"; "ocaml"; "use_libsoundness"]
-          (S[A"-ccopt"; A"-Lsrc/soundness"; A"-cclib"; A"-lsoundness"]);
+      (S[
+        (* following option is a workaround for a bug in ocaml/ocamlbuild 3.x *)
+        (* that reorders linker arguments *)
+        A"-cclib"; A"-Wl,--no-as-needed";
+        A"-cclib"; A"-lstdc++"
+        ]);
+
+    (* symbolically link in place the two static libraries needed for spot *)
+    (* this is done because we want to statically link them in *)
+    (* and ocamlbuild will just not let me do that with external paths *)
+    rule "libspot"
+      ~prod:"libspot.a"
+      (fun env _ -> Cmd(S[P"ln"; A"-s"; A(spot_path ^ "/lib/libspot.a"); A"."]));
+
+    rule "libbdd"
+      ~prod:"libbdd.a"
+      (fun env _ -> Cmd(S[P"ln"; A"-s"; A(spot_path ^ "/lib/libbdd.a"); A"."]));
+    
+    (* declare dependencies for things using libsoundness *)
+    dep ["link"; "ocaml"; "use_libsoundness"] 
+      [
+        (* the relative path to the .a file is necessary *)
+        "src/soundness/libsoundness.a";
+        "libspot.a";
+        "libbdd.a";
+      ];
+    (* 
+    flag ["link"; "ocaml"; "use_libsoundness"]
+      (S[A"-ccopt"; A"-Lsrc/soundness"; A"-cclib"; A"-lsoundness"]); *)
     
   | _ -> ()
 end
