25a6a883e31b6d96244263c13f349edfa0df2009
Reuben Rowe <r.rowe@ucl.ac.uk>
2014-10-03 12:44:44 +0100

    Removed some logically extraneous checks in the soundness checker

--- src/generic/soundcheck.ml
+++ src/generic/soundcheck.ml
@@ -165,9 +165,11 @@ let valid prf =
         Int.Map.mem i prf &&
         TagPairs.subset tp tv &&
         Tags.subset (TagPairs.projectl tv) (get_tags n) &&
-        Tags.subset (TagPairs.projectl tp) (get_tags n) &&
-        Tags.subset (TagPairs.projectr tv) (get_tags (Int.Map.find i prf)) &&
-        Tags.subset (TagPairs.projectr tp) (get_tags (Int.Map.find i prf))) 
+        (* Tags.subset (TagPairs.projectl tp) (get_tags n) && *) (* trivially true whenever tp a subset of tv *)
+        Tags.subset (TagPairs.projectr tv) (get_tags (Int.Map.find i prf)) 
+        (*   &&                                                                 *)  (* trivially true whenever tp a subset of tv *)
+        (* Tags.subset (TagPairs.projectr tp) (get_tags (Int.Map.find i prf))  *)
+        )
      (get_subg n))
     prf
 
