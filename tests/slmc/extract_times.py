641c262b3242b6336f42d78004ff07c85b0d8580
Reuben Rowe <r.rowe@ucl.ac.uk>
2015-07-08 20:39:46 +0100

    Added more model files for queue-02 test; reverted back to using time utility for gathering stats.

--- tests/slmc/extract_times.py
+++ tests/slmc/extract_times.py
@@ -5,8 +5,9 @@ import sys
 pattern = "Heap size: (\d+)\nGENERAL: Elapsed process time: (\d+) ms\n(?:.*\n){10}Model verified"
 
 input = sys.stdin.read()
-matches = re.finditer(pattern, input)
+matches = re.finditer("Heap size: (\d+)\nModel verified\n\nreal\t\d+m\d+\.\d+s\nuser\t(\d+)m(\d+\.\d+)s", input)
 for m in matches:
     heap_size = m.group(1)
-    milliseconds = m.group(2)
-    sys.stdout.write(", ".join((heap_size, milliseconds)) + "\n")
+    minutes = float(m.group(2))
+    seconds = float(m.group(3))
+    sys.stdout.write(", ".join((heap_size, str(minutes * 60 + seconds))) + "\n")
