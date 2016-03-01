698c5d8dfe14b9b8b861aa6fc3902ce72a081991
Nikos Gorogiannis <nikos.gorogiannis@gmail.com>
2016-03-01 16:04:19 +0000

    Simplified test case for last bug found.

--- tests/test_slterm_bug1.ml
+++ tests/test_slterm_bug1.ml
@@ -2,13 +2,9 @@ open Lib
 open Sl_term
 open Test
 
-let mk str = handle_reply (MParser.parse_string parse str ())
-
-
 let () =
   run 
-    "fresh existential is existential" 
+    "Empty set of variables must not raise exception in fresh_evar." 
     (fun () -> 
-      let xprime = fresh_evar (Set.empty) in
-      assert (is_exist_var xprime)
+      let _ = fresh_evar (Set.empty) in ()
     )  
