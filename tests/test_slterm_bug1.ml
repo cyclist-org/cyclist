0ccd82b3490500978f10be879f519db6e5468ee5
Nikos Gorogiannis <nikos.gorogiannis@gmail.com>
2016-07-14 10:48:49 +0100

    Dropped "Test" module due to probable name clashes with libraries.

--- tests/test_slterm_bug1.ml
+++ tests/test_slterm_bug1.ml
@@ -1,9 +1,8 @@
 open Lib
 open Sl_term
-open Test
 
 let () =
-  run 
+  runtest 
     "Empty set of variables must not raise exception in fresh_evar." 
     (fun () -> 
       let _ = fresh_evar (Set.empty) in ()
