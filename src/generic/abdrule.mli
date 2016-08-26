928da4ce3f5f4fd022c9245a5a2b9987ffcf2bf2
Nikos Gorogiannis <nikos.gorogiannis@gmail.com>
2016-08-26 15:24:41 +0100

    API documentation improvements.

--- src/generic/abdrule.mli
+++ src/generic/abdrule.mli
@@ -1,3 +1,5 @@
+(** An abduction rule used in an abductive cyclic prover. *)
+
 module Make(Seq : Sigs.SEQUENT)(Defs : Sigs.DEFS) : Sigs.ABDRULE
   with type seq_t = Seq.t
   with type proof_t = Proof.Make(Seq).t
--- src/generic/seqtactics.mli
+++ src/generic/seqtactics.mli
@@ -1,2 +1,4 @@
+(** Tactics for combining sequent-level rules. *)
+
 module Make(Seq : Sigs.SEQUENT) : Sigs.SEQTACTICS 
   with type seq_t = Seq.t
