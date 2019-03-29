2f02258b127e29ebdfbe5dd36d288aaea0268bf8
Nikos Gorogiannis <nikosgorogiannis@fb.com>
2019-03-29 13:31:56 +0000

    smtlib parser for prover

--- src/seplog/sl_prove.ml
+++ src/seplog/sl_prove.ml
@@ -9,7 +9,7 @@ let parse_null_as_emp = ref false
 (* switches controlling invalidity heuristic *)
 let invalidity_check = ref false
 
-let slcomp_mode = ref false
+let slcomp = ref ""
 
 module Prover = Prover.Make (Sl_seq)
 module F = Frontend.Make (Prover)
@@ -19,7 +19,8 @@ let () = F.maxbound := 0
 let () =
   F.usage :=
     !F.usage
-    ^ " [-D <file>] [-emp] [-Lem <int>] [-IC] [-IT] [-IP] [-SLCOMP] -S <string>"
+    ^ " [-D <file>] [-emp] [-Lem <int>] [-IC] [-IT] [-IP] [-SLCOMP file] [-S \
+       <string>]"
 
 let () =
   F.speclist :=
@@ -53,8 +54,9 @@ let () =
           , ": use partition strengthening in invalidity heuristic, default is "
             ^ string_of_bool !Sl_invalid.partition_strengthening )
         ; ( "-SLCOMP"
-          , Arg.Set slcomp_mode
-          , ": change output to sat/unsat/unknown for SLCOMP" )
+          , Arg.Set_string slcomp
+          , ": change input to SMTLIB <file> and output to sat/unsat/unknown \
+             for SLCOMP " ^ !slcomp )
         ; ( "-S"
           , Arg.Set_string cl_sequent
           , ": prove the SL sequent provided in <string>" ) ]
@@ -65,10 +67,15 @@ let () =
   Arg.parse spec_list
     (fun _ -> raise (Arg.Bad "Stray argument found."))
     !F.usage ;
-  if String.equal !cl_sequent "" then
+  let slcomp_mode = not (String.equal !slcomp "") in
+  if (not slcomp_mode) && String.equal !cl_sequent "" then
     F.die "-S must be specified." spec_list !F.usage ;
-  let seq = Sl_seq.of_string ~null_is_emp:!parse_null_as_emp !cl_sequent in
-  let defs = Sl_defs.of_channel (open_in !defs_path) in
+  let seq, defs =
+    if slcomp_mode then Sl_smtlib.of_channel (open_in !slcomp)
+    else
+      ( Sl_seq.of_string ~null_is_emp:!parse_null_as_emp !cl_sequent
+      , Sl_defs.of_channel (open_in !defs_path) )
+  in
   Sl_rules.setup defs ;
   let res =
     F.gather_stats (fun () ->
@@ -78,13 +85,13 @@ let () =
   match res with
   | Some None ->
       print_endline
-        ( if !slcomp_mode then "sat"
+        ( if slcomp_mode then "sat"
         else "NOT proved: " ^ Sl_seq.to_string seq ^ " [invalid]" ) ;
       exit 255
   | _ ->
       let res = Option.flatten res in
-      ( if !slcomp_mode then
+      if slcomp_mode then
         match res with
         | Some (Some _) -> print_endline "unsat"
-        | _ -> print_endline "unknown" ) ;
-      F.exit (F.process_result (not !slcomp_mode) seq res)
+        | _ -> print_endline "unknown"
+      else F.exit (F.process_result (not slcomp_mode) seq res)
