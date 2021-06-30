bdb2ce82b34dbef064e0581750afdcd051564b7c
Estibaliz <e.fraca@ucl.ac.uk>
2021-06-30 17:53:10 +0100

    Implementation of pheap.ml : a heap with permissions. Editing pheap.ml

--- src/seplog/pheap.ml
+++ src/seplog/pheap.ml
@@ -7,9 +7,10 @@ open MParser
 
 
 type psymheap = 
-  {permis: Q.t;
-  heap: Heap.t}
+  {permis: Q.t
+  ; heap: Heap.t}
  
+type t = psymheap
 
 (* accessors *)
 
@@ -17,5 +18,11 @@ let equal (h:psymheap) h' =
   h == h'
   || Q.equal h.permis h'.permis
   && Heap.equal h.heap h'.heap
-;;
-    
+ 
+  
+
+let compare f g = 
+  if f == g then 0
+  else Heap.compare f.heap g.heap
+ 
+
