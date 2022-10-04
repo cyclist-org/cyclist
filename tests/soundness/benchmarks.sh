e0fe7c2cecd5bb498849952186b5d526bc058075
Reuben Rowe <reuben.rowe@rhul.ac.uk>
2022-10-04 22:59:05 +0100

    Corrections to benchmark test script to incorporate (X)SD algorithms.

--- tests/soundness/benchmarks.sh
+++ tests/soundness/benchmarks.sh
@@ -10,10 +10,7 @@ fi
 now=$(date +"%Y-%m-%d_%H-%M-%S")
 sha=$(git rev-parse --short HEAD)
 
-opts=("-spot" "-rel-ext -full -ff" "-rel-ext -ff" "-rel-ext -min -ff" "-rel-ext -scc -ff" "-rel-ext -min -scc -ff")
-
-opts_with_sd=("${opts[@]}")
-opts_with_sd+=("-SD -XSD")
+base_opts=("-spot" "-rel-ext -full -ff" "-rel-ext -ff" "-rel-ext -min -ff" "-rel-ext -scc -ff" "-rel-ext -min -scc -ff")
 
 # Experiments run on Ubuntu 20.04.3, 16GB RAM
 # Experiements run with Spot 2.10.4
@@ -23,6 +20,9 @@ opts_with_sd+=("-SD -XSD")
 
 # -rel-ext -min -scc -ff n = 450 ~60s
 # -rel-ext -min -scc -ff n = 457 OUT OF MEMORY (before 9e40c42)
+
+opts=("${base_opts[@]}")
+opts+=("-SD -XSD")
 
 inputs="1 $(seq 25 25 450)"
 for n in ${inputs[@]}; do
@@ -45,10 +45,13 @@ done
 # -rel-ext -min -scc -ff n = 375 ~475s
 # -rel-ext -min -scc -ff n = 379 OUT OF MEMORY (before 9e40c42)
 
+opts=("${base_opts[@]}")
+opts+=("-XSD")
+
 inputs="1 $(seq 20 20 180)"
 for n in ${inputs[@]}; do
   for i in ${!opts[@]}; do
-    opt=${opts_with_sd[i]}
+    opt=${opts[i]}
     suffix=$(echo "$opt" | sed -r 's/\s+//g')
     for j in $(seq 1 1 $iterations); do
       (set -x; time dune exec --context=test2-benchmarks tests/soundness/test2.exe -- $opt -rel-stats $n) >> "$logdir/test2_benchmarks_${sha}_${now}$suffix.$j.log" 2>&1
@@ -60,6 +63,8 @@ done
 # test3.exe - negative example
 
 # -rel-ext -min -scc -ff n = 21639 OUT OF MEMORY (before 9e40c42)
+
+opts=("${base_opts[@]}")
 
 inputs="1 $(seq 250 250 4750)"
 for n in ${inputs[@]}; do
