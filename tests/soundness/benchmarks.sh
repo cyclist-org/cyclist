3d63dc2258e705dfc8c9ea818f947f7a89ad4438
Reuben Rowe <reuben.rowe@rhul.ac.uk>
2022-10-12 14:58:36 +0100

    Update for ESOP2023 paper submission.

--- tests/soundness/benchmarks.sh
+++ tests/soundness/benchmarks.sh
@@ -10,7 +10,10 @@ fi
 now=$(date +"%Y-%m-%d_%H-%M-%S")
 sha=$(git rev-parse --short HEAD)
 
-base_opts=("-spot" "-rel-ext -full -ff" "-rel-ext -ff" "-rel-ext -min -ff" "-rel-ext -scc -ff" "-rel-ext -min -scc -ff")
+base_opts=("-spot" "-rel-ext -min -scc -ff")
+
+xtd_opts=("${base_opts[@]}")
+xtd_opts+=("-rel-ext -full -ff" "-rel-ext -ff" "-rel-ext -min -ff" "-rel-ext -scc -ff")
 
 # Experiments run on Ubuntu 20.04.3, 16GB RAM
 # Experiements run with Spot 2.10.4
@@ -22,9 +25,10 @@ base_opts=("-spot" "-rel-ext -full -ff" "-rel-ext -ff" "-rel-ext -min -ff" "-rel
 # -rel-ext -min -scc -ff n = 457 OUT OF MEMORY (before 9e40c42)
 
 opts=("${base_opts[@]}")
-opts+=("-SD" "-XSD")
+# SD and XSD run out of memory at about 16 <= n <= 26
+# opts+=("-SD" "-XSD")
 
-inputs="1 $(seq 25 25 450)"
+inputs="1 $(seq 25 25 600)"
 for n in ${inputs[@]}; do
   for i in ${!opts[@]}; do
     opt=${opts[i]}
@@ -46,9 +50,10 @@ done
 # -rel-ext -min -scc -ff n = 379 OUT OF MEMORY (before 9e40c42)
 
 opts=("${base_opts[@]}")
-opts+=("-XSD")
+# XSD displays exponential behaviour (1s for p = 8, 15s for p = 9, 374s for p = 10)
+# opts+=("-XSD")
 
-inputs="1 $(seq 20 20 180)"
+inputs="1 $(seq 25 25 300)"
 for n in ${inputs[@]}; do
   for i in ${!opts[@]}; do
     opt=${opts[i]}
@@ -66,7 +71,7 @@ done
 
 opts=("${base_opts[@]}")
 
-inputs="1 $(seq 250 250 4750)"
+inputs="1 $(seq 250 250 4000)"
 for n in ${inputs[@]}; do
   for i in ${!opts[@]}; do
     opt=${opts[i]}
