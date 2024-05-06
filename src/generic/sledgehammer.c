2481186a0a31076b42d44dc6c36c5d841c6eba37
matan shaked <s.matan21@gmail.com>
2024-05-06 20:04:44 +0300

    add in progress sledgehammer test

--- src/generic/sledgehammer.c
+++ src/generic/sledgehammer.c
@@ -7,14 +7,17 @@
 #include "criterion.fwk.c"
 #include "criterion.order_reduced.c"
 #include "criterion.flat_cycles.c"
+#include "criterion.flat_cycles.generalized.c"
+// #include "criterion.no_flat_extended_cycles.c"
 #include <future>
+#include <unistd.h>
 
-Sledgehammer::Sledgehammer(Heighted_graph *hg, Heighted_graph::NODE_ORDER order, int opts)
+Sledgehammer::Sledgehammer(Heighted_graph *hg, Heighted_graph::NODE_ORDER order, int opts) : hg(hg), order(order), opts(opts)
 {
-    this->soundness_criteria.push_back(std::make_shared<VlaCriterion>(hg));
-    this->soundness_criteria.push_back(std::make_shared<SlaCriterion>(hg));
-    this->soundness_criteria.push_back(std::make_shared<OrderReducedCriterion>(hg, order, opts));
-    this->soundness_criteria.push_back(std::make_shared<FlatCyclesCriterion>(hg));
+    // this->soundness_criteria.push_back(std::make_shared<VlaCriterion>(hg));
+    // this->soundness_criteria.push_back(std::make_shared<SlaCriterion>(hg));
+    // this->soundness_criteria.push_back(std::make_shared<OrderReducedCriterion>(hg, order, opts));
+    // this->soundness_criteria.push_back(std::make_shared<FlatCyclesCriterion>(hg));
 }
 
 Sledgehammer::~Sledgehammer()
@@ -23,10 +26,59 @@ Sledgehammer::~Sledgehammer()
 
 bool Sledgehammer::check_soundness()
 {
+    std::string graph_str = this->hg->to_string();
+    // this->hg->remove_down_edges_not_in_any_SCC();
+
+    // flat cycles: complete unsound
+    auto flat_cycles_start = std::chrono::system_clock::now();
+    
+    // FlatCyclesCriterion flat_cycles_criterion(this->hg);
+    // auto resultFC = flat_cycles_criterion.check_soundness();
+    
+    GeneralizedFlatCyclesCriterion generalized_flat_cycles_criterion(this->hg);
+    auto resultFC = generalized_flat_cycles_criterion.check_soundness();
+
+    auto flat_cycles_end = std::chrono::system_clock::now();
+    if (resultFC == SoundnessCheckResult::unsound)
+    {
+        printf("FC returned true negative after %lu us\n", flat_cycles_end-flat_cycles_start);
+        fflush(stdout);
+        // printf("unsound %s\n", graph_str.c_str());
+        // fflush(stdout);
+        return false;
+    }
+    
+    // no flat extended cycles: sound incomplete
+    // auto no_flat_extended_cycles_start = std::chrono::system_clock::now();
+    // NoFlatExtendedCyclesCriterion no_flat_extended_cycles_criterion(this->hg);
+    // auto no_flat_extended_cycles_end = std::chrono::system_clock::now();
+    // if (no_flat_extended_cycles_criterion.check_soundness() == SoundnessCheckResult::sound)
+    // {
+    //     printf("NFEC returned true positive after %lu us\n", no_flat_extended_cycles_end - no_flat_extended_cycles_start);
+    //     fflush(stdout);
+    //     printf("sound %s\n", this->hg->to_string().c_str());
+    //     fflush(stdout);
+    //     return true;
+    // }
+
+    auto OR_start = std::chrono::system_clock::now();
+    auto result1 = this->hg->order_reduced_check(this->order, this->opts);
+    auto OR_end = std::chrono::system_clock::now();
+    printf("OR returned after %lu us\n", OR_end - OR_start);
+    // printf("FC+OR returned after %lu us\n", OR_end - flat_cycles_start);
+    fflush(stdout);
+
+    // printf("%s %s\n", result1 ? "sound" : "unsound", graph_str.c_str());
+    // fflush(stdout);
+
+    return result1;
+
     this->start_all_criteria();
     bool result = this->wait_for_any_future();
     this->halt_all_criteria();
+
     return result;
+    // return this->hg->order_reduced_check(this->order, this->opts);
 }
 
 void Sledgehammer::start_all_criteria()
@@ -35,6 +87,7 @@ void Sledgehammer::start_all_criteria()
     {
         this->soundness_results_futures.push_back(
             std::async(
+                std::launch::async,
                 &SoundnessCriterion::check_soundness,
                 this->soundness_criteria[i]));
     };
@@ -56,21 +109,20 @@ bool Sledgehammer::wait_for_any_future()
             }
             std::future<SoundnessCheckResult> *curr_future = &(this->soundness_results_futures[i]);
             std::future_status curr_future_status =
-                curr_future->wait_for(std::chrono::microseconds(FUTURE_WAIT_TIME_MS));
+                curr_future->wait_for(std::chrono::milliseconds(FUTURE_WAIT_TIME_MS));
             if (curr_future_status == std::future_status::ready)
             {
                 criteria_that_are_done.insert(i);
                 auto curr_result = curr_future->get();
                 if (curr_result != SoundnessCheckResult::dontKnow)
                 {
-                    // printf("criterion number %zu done first\n", i);
+                    // printf("criterion %s done first\n", this->soundness_criteria[i]->get_criterion_name().c_str());
                     return curr_result == SoundnessCheckResult::sound;
                 }
-                // printf("criterion number %zu done but doesn't know\n", i);
             }
         }
     }
-    throw "no criterion returned an answer";
+    throw std::runtime_error("no criterion returned an answer");
 }
 
 void Sledgehammer::halt_all_criteria()
--- src/generic/sledgehammer.hpp
+++ src/generic/sledgehammer.hpp
@@ -12,12 +12,15 @@ class Sledgehammer
 {
 private:
     Heighted_graph *hg;
+    int opts;
+    Heighted_graph::NODE_ORDER order;
     Vec<std::shared_ptr<SoundnessCriterion>> soundness_criteria;
     Vec<std::future<SoundnessCheckResult>> soundness_results_futures;
 
     void start_all_criteria();
     bool wait_for_any_future();
     void halt_all_criteria();
+
 
 public:
     Sledgehammer(Heighted_graph *hg, Heighted_graph::NODE_ORDER order, int opts);
