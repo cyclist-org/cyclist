359c2d7ec3041b8269ebf7fa1e63ce618591bbe3
Reuben Rowe <reuben.rowe@rhul.ac.uk>
2024-06-01 16:24:52 +0100

    Remove --batch option and always read stdin character by character, since trying to parse stdin channel directly raises an exception; add -f option to read directly from files instead.

--- src/generic/checkproof.ml
+++ src/generic/checkproof.ml
@@ -265,18 +265,20 @@ type input_mode_t =
 
 let input_mode = ref EDGE_LIST
 
+let input_files = ref []
+
 let speclist =
   Soundcheck.arg_opts
     @
   [
     ("-d", Arg.Set do_debug, ": print debug messages") ;
     ("--json", Arg.Unit (fun () -> input_mode := JSON), "Use JSON input format") ;
-    ("--batch", Arg.Set batch_mode, ": batch processing mode") ;
     ("--allow-comments", Arg.Set allow_comments, ": allow line comments in input") ;
+    ("-f", Arg.String (fun f -> input_files := f :: !input_files), ": take input from file") ;
   ]
 
 let usage =
-  "usage: " ^ Sys.argv.(0) ^ " [-d] [-s] [-spot|-rel-ext|SD [-full][-ff][-scc][-idem][-min][-rel-stats]]"
+  "usage: " ^ Sys.argv.(0) ^ " [-d] [-s] [--json] [ --allow-comments | (-f <file>)* ] [ -VLA | -SLA | -legacy | -OR | -FWK [-ff][-scc][-idem][-min][-rel-stats] ]"
 
 let () =
   Arg.parse speclist (fun _ -> ()) usage
@@ -287,61 +289,68 @@ let do_check (prf, init) =
   else
     print_endline "NO"
 
+let process_files parser =
+  let process_file f =
+    let f_in = open_in f in
+    let prfs =
+      handle_reply (parse_channel parser f_in ()) in
+    let () = close_in f_in in
+    List.iter do_check prfs in
+  List.iter process_file (List.rev !input_files)
+
+let process_stdin parser =
+  let buf = Buffer.create 2014 in
+  let next_char =
+    match !allow_comments with
+    | true ->
+      let comment = ref false in
+      (fun () ->
+        let c = input_char stdin in
+        let () =
+          if not !comment && Char.equal c '#'
+            then comment := true
+          else if Char.equal c '\n'
+            then comment := false in
+        let () =
+          if not !comment then Buffer.add_char buf c in
+        not !comment && Char.equal c ';')
+    | false ->
+      (fun () ->
+        let c = input_char stdin in
+        let () = Buffer.add_char buf c in
+        Char.equal c ';')
+    in
+  while true do
+    let ready =
+      try
+        next_char ()
+      with End_of_file -> true in
+    if ready then
+      let input = Buffer.contents buf in
+      let () = Buffer.clear buf in
+      match (parse_string (spaces >> eof) input ()) with
+      | Success _ ->
+        (* if input consists of nothing but spaces *)
+        exit 0
+      | Failed _ ->
+        let prfs =
+          handle_reply (parse_string parser input ()) in
+        List.iter do_check prfs
+    done
+
 let () =
   let () = gc_setup () in
   let () = Format.set_margin (Sys.command "exit $(tput cols)") in
-  match !batch_mode, !allow_comments with
-  | true, false ->
-    let parser =
-      match !input_mode with
-      | EDGE_LIST ->
-        many (spaces >> parse_proof)
-      | JSON ->
-        many (spaces >> parse_json_proof)
-      in
-    let prfs =
-      handle_reply (parse_channel parser stdin ()) in
-    List.iter do_check prfs
+  let parser =
+    match !input_mode with
+    | EDGE_LIST ->
+      many (spaces >> parse_proof)
+    | JSON ->
+      sep_end_by
+        (spaces >> parse_json_proof << spaces)
+        ((skip Tokens.semi << spaces) <|> spaces) in
+  match !input_files with
+  | [] ->
+    process_stdin parser
   | _ ->
-    let buf = Buffer.create 2014 in
-    let next_char =
-      match !allow_comments with
-      | true ->
-        let comment = ref false in
-        (fun () ->
-          let c = input_char stdin in
-          let () =
-            if not !comment && Char.equal c '#'
-              then comment := true
-            else if Char.equal c '\n'
-              then comment := false in
-          let () =
-            if not !comment then Buffer.add_char buf c in
-          not !comment && Char.equal c ';')
-      | false ->
-        (fun () ->
-          let c = input_char stdin in
-          let () = Buffer.add_char buf c in
-          Char.equal c ';')
-      in
-    while true do
-      let ready =
-        try
-          next_char ()
-        with End_of_file -> true in
-      if ready then
-        let input = Buffer.contents buf in
-        let () = Buffer.clear buf in
-        match (parse_string (spaces >> eof) input ()) with
-        | Success _ ->
-          (* if input consists of nothing but spaces *)
-          exit 0
-        | Failed _ ->
-          let parser =
-            match !input_mode with
-            | EDGE_LIST ->
-              spaces >> parse_proof
-            | JSON ->
-              spaces >> parse_json_proof << spaces << Tokens.semi in
-          do_check (handle_reply (parse_string parser input ()))
-    done
+    process_files parser
