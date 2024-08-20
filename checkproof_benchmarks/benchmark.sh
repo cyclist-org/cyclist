95d80911d861193df5d04334417efe265b5d4811
matan shaked <s.matan21@gmail.com>
2024-08-20 14:34:16 +0300

    add benchmarking scripts

--- checkproof_benchmarks/benchmark.sh
+++ checkproof_benchmarks/benchmark.sh
@@ -1,23 +1,60 @@
+
+
 cd ../
 out_dir="./checkproof_benchmarks/logs"
-for method in "SH" "OR" "FWK"; do
-    for i in {1..5}; do
-        dune exec src/generic/checkproof.exe -- -"$method" -min -scc -ff -s --json < ../cyclist_graphs/unminimized/all_graphs.json  > "$out_dir/results.minimization.$method.$i"
-    done
-done
-for method in "SH" "OR" "FWK"; do
-    for i in {1..5}; do
-        dune exec src/generic/checkproof.exe -- -"$method" -min -scc -ff -s --json --unminimized-proofs < ../cyclist_graphs/unminimized/all_graphs.json  > "$out_dir/results.no_minimization.$method.$i"
-    done
+
+fo_graphs_dir="../cyclist_graphs/cyclist_output_graphs/fo"
+sl_graphs_dir="../cyclist_graphs/cyclist_output_graphs/sl"
+
+
+for i in {1..3}; do
+    echo "running $i..."
+    find ../cyclist_graphs/cyclist_output_graphs/sl -maxdepth 1 -name "*.json" | sort | xargs -I {} echo -f {} | xargs dune exec src/generic/checkproof.exe -- -SH -min -scc -ff -s -R json  > "$out_dir/results.TM.sl.$i" 
+    find ../cyclist_graphs/cyclist_output_graphs/fo -maxdepth 1 -name "*.json" | sort | xargs -I {} echo -f {} | xargs dune exec src/generic/checkproof.exe -- -SH -min -scc -ff -s -R json  > "$out_dir/results.TM.fo.$i" 
+    echo done $i
 done
 
-for method in "VLA" "SLA"; do
-    for i in {1..2}; do
-        dune exec src/generic/checkproof.exe -- -"$method" -s --json < ../cyclist_graphs/unminimized/all_graphs.json  > "$out_dir/results.minimization."$method".$i"
-    done
+
+# for method in "OR" "FWK"; do
+#     for i in {1..5}; do
+#         find ../cyclist_graphs/cyclist_output_graphs/sl -maxdepth 1 -name "*.json" | sort | xargs -I {} echo -f {} | xargs dune exec src/generic/checkproof.exe -- -"$method" -min -scc -ff -s -R json  > "$out_dir/results.$method.sl.$i" 
+#         find ../cyclist_graphs/cyclist_output_graphs/fo -maxdepth 1 -name "*.json" | sort | xargs -I {} echo -f {} | xargs dune exec src/generic/checkproof.exe -- -"$method" -min -scc -ff -s -R json  > "$out_dir/results.$method.fo.$i" 
+#     done
+# done
+# for method in "SLA" "VLA"; do
+#     for i in {1..3}; do
+#         find ../cyclist_graphs/cyclist_output_graphs/sl -maxdepth 1 -name "*.json" | sort | xargs -I {} echo -f {} | xargs dune exec src/generic/checkproof.exe -- -"$method" -s -R json  > "$out_dir/results.$method.sl.$i" 
+#         find ../cyclist_graphs/cyclist_output_graphs/fo -maxdepth 1 -name "*.json" | sort | xargs -I {} echo -f {} | xargs dune exec src/generic/checkproof.exe -- -"$method" -s -R json  > "$out_dir/results.$method.fo.$i" 
+#     done
+# done
+
+for i in {1..3}; do
+    find ../cyclist_graphs/cyclist_output_graphs/sl -maxdepth 1 -name "*.json" | sort | xargs -I {} echo -f {} | xargs dune exec src/generic/checkproof.exe -- -"SLA" -s -R json  > "$out_dir/results.SLA.sl.$i" 
+    find ../cyclist_graphs/cyclist_output_graphs/fo -maxdepth 1 -name "*.json" | sort | xargs -I {} echo -f {} | xargs dune exec src/generic/checkproof.exe -- -"SLA" -s -R json  > "$out_dir/results.SLA.fo.$i" 
 done
-for method in "VLA" "SLA"; do
-    for i in {1..2}; do
-        dune exec src/generic/checkproof.exe -- -"$method" -s --json --unminimized-proofs < ../cyclist_graphs/unminimized/all_graphs.json  > "$out_dir/results.no_minimization."$method".$i"
-    done
-done
+
+# for method in "SH" "OR" "FWK"; do
+#     for i in {1..5}; do
+#         dune exec src/generic/checkproof.exe -- -"$method" -min -scc -ff -s --json --unminimized-proofs < ../cyclist_graphs/unminimized/all_graphs.json  > "$out_dir/results.no_minimization.$method.$i"
+#     done
+# done
+
+# for method in "VLA" "SLA"; do
+#     for i in {1..2}; do
+#         dune exec src/generic/checkproof.exe -- -"$method" -s --json < ../cyclist_graphs/unminimized/all_graphs.json  > "$out_dir/results.minimization."$method".$i"
+#     done
+# done
+# for method in "VLA" "SLA"; do
+#     for i in {1..2}; do
+#         dune exec src/generic/checkproof.exe -- -"$method" -s --json --unminimized-proofs < ../cyclist_graphs/unminimized/all_graphs.json  > "$out_dir/results.no_minimization."$method".$i"
+#     done
+# done
+
+
+
+
+# single
+
+# cd ../
+# out_dir="./checkproof_benchmarks/logs"
+# dune exec src/generic/checkproof.exe -- -SH -min -scc -ff -s --unminimized-proofs --json < ../cyclist_graphs/unminimized/all_graphs.json  > "$out_dir/results.no_minimization.SH+DU.optimized_FC"
--- checkproof_benchmarks/parse_bench.py
+++ checkproof_benchmarks/parse_bench.py
@@ -3,7 +3,8 @@ import csv
 import numpy as np
 import functools
 from matplotlib import pyplot as plt
-
+import seaborn as sns
+import pandas as pd
 
 def get_minimization_times(minimization_contents):
     return list(map(float,re.findall(r"Minimization took: (\d+\.\d+) ms", minimization_contents)))
@@ -16,8 +17,8 @@ def parse(contents):
 
 def parse_files(filepaths):
     files_contents = list(map(lambda path: open(path).read(), filepaths))
-    minimization_times = np.around(functools.reduce(lambda x,y: x+y, map(lambda contents: np.array(get_minimization_times(contents)), files_contents))/len(filepaths), decimals=4)
-    modelcheck_times = np.around(functools.reduce(lambda x,y: x+y, map(lambda contents: np.array(get_modelcheck_times(contents)), files_contents))/len(filepaths), decimals=4)
+    minimization_times = (functools.reduce(lambda x,y: x+y, map(lambda contents: np.array(get_minimization_times(contents)), files_contents))/len(filepaths))*1000
+    modelcheck_times = (functools.reduce(lambda x,y: x+y, map(lambda contents: np.array(get_modelcheck_times(contents)), files_contents))/len(filepaths))*1000
     soundness = get_soundness(files_contents[0])
     return soundness, minimization_times.tolist(), modelcheck_times.tolist()
     # return list(zip(soundness, minimization_times, modelcheck_times))
@@ -57,124 +58,185 @@ def generate_tables(parsed_minimization_unzipped_SH, parsed_no_minimization_unzi
 
 def output_to_csv(table_minimized, table_nonminimized):
     # table = [(*SH_tuple_min, OR_tuple_min[2], VLA_tuple_min[2], SH_tuple_no_min[2], OR_tuple_no_min[2], VLA_tuple_no_min[2],*graph_stats) for SH_tuple_min, SH_tuple_no_min, OR_tuple_min, OR_tuple_no_min, VLA_tuple_min, VLA_tuple_no_min, graph_stats in zip(parsed_minimization_SH, parsed_no_minimization_SH, parsed_minimization_OR, parsed_no_minimization_OR, parsed_minimization_VLA, parsed_no_minimization_VLA, parsed_stats)]
-    csv_file = open("minimized.csv", mode='w')
+    # csv_file = open("minimized.csv", mode='w')
+    csv_file = open("minimized.opt.csv", mode='w')
     writer = csv.writer(csv_file)
 
-    writer.writerow(["is sound", "minimization time (ms)", "minimized model check time SH (ms)", "minimized model check time OR (ms)", "minimized model check time FWK (ms)", "minimized model check time VLA (ms)", "minimized model check time SLA (ms)", "amount of nodes", "amount of edges", "amount of backedges", "has overlapping cycles", "amount of positions", "graph width", "filename"])
+    writer.writerow(["is sound", "minimization time (ms)", "minimized model check time CY (ms)", "minimized model check time OR (ms)", "minimized model check time FWK (ms)", "minimized model check time VLA (ms)", "minimized model check time SLA (ms)", "amount of nodes", "amount of edges", "amount of backedges", "has overlapping cycles", "amount of positions", "graph width", "filename"])
     for row in table_minimized:
         writer.writerow(row)
 
     csv_file.close()
 
-    csv_file = open("non-minimized.csv", mode='w')
+    # csv_file = open("non-minimized.csv", mode='w')
+    csv_file = open("non-minimized.opt.csv", mode='w')
     writer = csv.writer(csv_file)
 
-    writer.writerow(["is sound", "minimization time (ms)", "non-minimized model check time SH (ms)", "non-minimized model check time OR (ms)", "non-minimized model check time VLA (ms)", "amount of nodes", "amount of edges", "amount of backedges", "has overlapping cycles", "amount of positions", "graph_width", "filename"])
+    writer.writerow(["is sound", "minimization time (ms)", "non-minimized model check time CY (ms)", "non-minimized model check time OR (ms)", "non-minimized model check time VLA (ms)", "amount of nodes", "amount of edges", "amount of backedges", "has overlapping cycles", "amount of positions", "graph_width", "filename"])
     for row in table_nonminimized:
         writer.writerow(row)
 
     csv_file.close()
 
-    # minimization_table = [(*SH_tuple, OR_tuple[2], *t2) for SH_tuple, OR_tuple,t2 in zip(parsed_minimization_SH, parsed_minimization_OR, parsed_stats)]
-    # no_minimization_table = [(*SH_tuple, OR_tuple[2], *t2) for SH_tuple, OR_tuple,t2 in zip(parsed_no_minimization_SH, parsed_no_minimization_OR, parsed_stats)]
 
-    # csv_file = open("minimization.csv", mode='w')
-    # writer = csv.writer(csv_file)
-
-    # writer.writerow(["is sound", "minimization time (ms)", "model check time with Sledgehammer (ms)", "model check time with Order Reduced (ms)", "amount of nodes", "amount of edges", "amount of backedges", "amount of positions", "filename"])
-    # for row in minimization_table:
-    #     writer.writerow(row)
-
-    # csv_file.close()
-
-    # csv_file = open("no_minimization.csv", mode='w')
-    # writer = csv.writer(csv_file)
-
-    # writer.writerow(["is sound", "minimization time (ms)", "model check time with Sledgehammer (ms)", "model check time with Order Reduced (ms)", "amount of nodes", "amount of edges", "amount of backedges", "amount of positions", "filename"])
-    # for row in no_minimization_table:
-    #     writer.writerow(row)
-
-def to_numeric(table):
-    return [
-        (
-            float(1 if tup[0] else 0),
-            float(tup[1]),
-            float(tup[2]),
-            float(tup[3]),
-            float(tup[4]),
-            float(tup[5]),
-            float(tup[6]),
-            float(tup[7]),
-            float(1 if tup[8] else 0),
-            float(tup[9]),
-            float(tup[10])
-        )
-        for tup in table
-    ]
-
-from sklearn.decomposition import PCA
-from sklearn.cluster import KMeans
-def analyze_table(table):
-    numric_table = to_numeric(table)
-    matrix = np.array(numric_table)
-
-    kmeans = KMeans(n_clusters=5)
-    kmeans.fit(matrix)
-
-    cluster_centers = kmeans.cluster_centers_
-
-    # Get the labels assigned to each data point
-    labels = kmeans.labels_
-
-    # Count the number of points in each cluster
-    unique, counts = np.unique(labels, return_counts=True)
-    points_per_cluster = dict(zip(unique, counts))
-
-    print("Number of points in each cluster:")
-    for cluster, count in points_per_cluster.items():
-        print(f"Cluster {cluster}: {count} points")
-    print([
-        [
-            round(float(entry), 5) for entry in component
-        ]
-        for component in kmeans.cluster_centers_
-    ])
-
-    # pca = PCA(n_components=5)
-    # print("calculating PCs...")
-    # pca.fit(matrix)
-    # print(pca.explained_variance_ratio_)
-    # print([
-    #     [
-    #         round(float(entry), 5) for entry in component
-    #     ]
-    #     for component in pca.components_
-    # ])
-    
-
-minimization_filepaths_SH = [f"./logs/results.minimization.SH.{i}" for i in range(1,6)]
-no_minimization_filepaths_SH = [f"./logs/results.no_minimization.SH.{i}" for i in range(1,6)]
-minimization_filepaths_OR = [f"./logs/results.minimization.OR.{i}" for i in range(1,6)]
-no_minimization_filepaths_OR = [f"./logs/results.no_minimization.OR.{i}" for i in range(1,6)]
-minimization_filepaths_FWK = [f"./logs/results.minimization.FWK.{i}" for i in range(1,6)]
+# minimization_filepaths_SH = [f"./logs/results.minimization.CY.{i}" for i in range(1,6)]
+# no_minimization_filepaths_SH = [f"./logs/results.no_minimization.CY.{i}" for i in range(1,6)]
+# minimization_filepaths_OR = [f"./logs/results.minimization.OR.{i}" for i in range(1,6)]
+# no_minimization_filepaths_OR = [f"./logs/results.no_minimization.OR.{i}" for i in range(1,6)]
+# minimization_filepaths_FWK = [f"./logs/results.minimization.FWK.{i}" for i in range(1,6)]
 # no_minimization_filepaths_FWK = [f"./logs/results.no_minimization.FWK.{i}" for i in range(1,6)]
-minimization_filepaths_VLA = [f"./logs/results.minimization.VLA.{i}" for i in range(1,3)]
-no_minimization_filepaths_VLA = [f"./logs/results.no_minimization.VLA.{i}" for i in range(1,3)]
-minimization_filepaths_SLA = [f"./logs/results.minimization.SLA.{i}" for i in range(1,3)]
+# minimization_filepaths_VLA = [f"./logs/results.minimization.VLA.{i}" for i in range(1,3)]
+# no_minimization_filepaths_VLA = [f"./logs/results.no_minimization.VLA.{i}" for i in range(1,3)]
+# minimization_filepaths_SLA = [f"./logs/results.minimization.SLA.{i}" for i in range(1,3)]
 # no_minimization_filepaths_SLA = [f"./logs/results.no_minimization.SLA.{i}" for i in range(1,3)]
 
-parsed_minimization_unzipped_SH = parse_files(minimization_filepaths_SH)
-parsed_no_minimization_unzipped_SH = parse_files(no_minimization_filepaths_SH)
-parsed_minimization_unzipped_OR = parse_files(minimization_filepaths_OR)
-parsed_no_minimization_unzipped_OR = parse_files(no_minimization_filepaths_OR)
-parsed_minimization_unzipped_FWK = parse_files(minimization_filepaths_FWK)
-parsed_minimization_unzipped_VLA = parse_files(minimization_filepaths_VLA)
-parsed_no_minimization_unzipped_VLA = parse_files(no_minimization_filepaths_VLA)
-parsed_minimization_unzipped_SLA = parse_files(minimization_filepaths_SLA)
+# parsed_minimization_unzipped_SH = parse_files(minimization_filepaths_SH)
+# parsed_no_minimization_unzipped_SH = parse_files(no_minimization_filepaths_SH)
+# parsed_minimization_unzipped_SH_DU = parse_files(minimization_filepaths_SH_DU)
+# parsed_no_minimization_unzipped_SH_DU = parse_files(no_minimization_filepaths_SH_DU)
+
+# parsed_minimization_unzipped_OR = parse_files(minimization_filepaths_OR)
+# parsed_no_minimization_unzipped_OR = parse_files(no_minimization_filepaths_OR)
+# parsed_minimization_unzipped_FWK = parse_files(minimization_filepaths_FWK)
+# parsed_minimization_unzipped_VLA = parse_files(minimization_filepaths_VLA)
+# parsed_no_minimization_unzipped_VLA = parse_files(no_minimization_filepaths_VLA)
+# parsed_minimization_unzipped_SLA = parse_files(minimization_filepaths_SLA)
 
 
-table_minimized, table_nonminimized = generate_tables(parsed_minimization_unzipped_SH, parsed_no_minimization_unzipped_SH, parsed_minimization_unzipped_OR, parsed_no_minimization_unzipped_OR, parsed_minimization_unzipped_FWK, parsed_minimization_unzipped_VLA, parsed_no_minimization_unzipped_VLA, parsed_minimization_unzipped_SLA)
-# analyze_table(table_minimized)
-# analyze_table(table_nonminimized)
-output_to_csv(table_minimized, table_nonminimized)
+# table_minimized, table_nonminimized = generate_tables(parsed_minimization_unzipped_SH_DU, parsed_no_minimization_unzipped_SH_DU, parsed_minimization_unzipped_OR, parsed_no_minimization_unzipped_OR, parsed_minimization_unzipped_FWK, parsed_minimization_unzipped_VLA, parsed_no_minimization_unzipped_VLA, parsed_minimization_unzipped_SLA)
+# output_to_csv(table_minimized, table_nonminimized)
 
+# table_minimized, table_nonminimized = generate_DU_comparison_tables(parsed_minimization_unzipped_SH, parsed_no_minimization_unzipped_SH, parsed_minimization_unzipped_SH_DU, parsed_no_minimization_unzipped_SH_DU)
+# output_comparison_tables_to_csv(table_minimized, table_nonminimized)
+
+def get_graphs_stats(csv_filepath):
+    return pd.read_csv(csv_filepath)
+
+def plot_mean_with_interquartile_range_by(frame, group_by_col, values_col, show_legend=True):
+    means = frame.groupby(group_by_col)[values_col].mean()
+    stats = frame.groupby(group_by_col)[values_col].describe()
+    quartiles1 = stats["25%"]
+    quartiles3 = stats["75%"]
+    mins = frame.groupby(group_by_col)[values_col].min()
+    maxs = frame.groupby(group_by_col)[values_col].max()
+    ax = sns.lineplot(x=stats.index, y=means, label=values_col)
+    ax.fill_between(stats.index, quartiles1, quartiles3, alpha=0.3)
+    # ax.set_yscale("log")
+    if show_legend:
+        ax.legend()
+    ax.set_ylabel("duration (milliseconds)")
+
+def get_method_runtimes(runs_filepaths_prefixes, amount_of_runs, method_description):
+    filepaths_sl = [f"{runs_filepaths_prefixes}.sl.{i}" for i in range(1,amount_of_runs+1)]
+    filepaths_fo = [f"{runs_filepaths_prefixes}.fo.{i}" for i in range(1,amount_of_runs+1)]
+    
+    parsed_sl = parse_files(filepaths_sl)
+    parses_fo = parse_files(filepaths_fo)
+
+    return pd.DataFrame({
+        method_description: parses_fo[2]+parsed_sl[2]
+    })
+
+
+def compare_SH_with_and_without_TM(frame):
+    plot_mean_with_interquartile_range_by(frame, "edges", "VLA")
+    # plot_mean_with_interquartile_range_by(frame, "edges", "SLA")
+    plot_mean_with_interquartile_range_by(frame, "edges", "FWK")
+    plot_mean_with_interquartile_range_by(frame, "edges", "OR")
+    plot_mean_with_interquartile_range_by(frame, "edges", "CY")
+    # plot_mean_with_interquartile_range_by(frame, "edges", "CY with TM")
+    # plot_mean_with_interquartile_range_by(frame, "amount of backedges", "CY with TM")
+    # plot_mean_with_interquartile_range_by(frame, "amount of backedges", "VLA")
+    # plot_mean_with_interquartile_range_by(frame, "amount of backedges", "SLA", show_legend=False)
+    # plot_mean_with_interquartile_range_by(frame, "amount of backedges", "FWK")
+    # plot_mean_with_interquartile_range_by(frame, "amount of backedges", "OR")
+    # plot_mean_with_interquartile_range_by(frame, "amount of backedges", "CY")
+    plt.show()
+    
+    frame = frame[(frame["FC answers"]!="don't know")|(frame["DU answers"]!="don't know")|(frame["TM answers"]!="don't know")]
+    print(sum(frame["OR"]))
+    print(sum(frame["CY"]))
+    print(sum(frame["CY with TM"]))
+    print(len(frame.index))
+    # plot_mean_with_interquartile_range_by(frame, "edges", "OR")
+    # plot_mean_with_interquartile_range_by(frame, "edges", "CY")
+    # plot_mean_with_interquartile_range_by(frame, "edges", "CY with TM")
+    # plt.show()
+
+def plot_CY_performance(frame):
+    # plot_mean_with_interquartile_range_by(frame, "edges", "OR")
+    # plot_mean_with_interquartile_range_by(frame, "edges", "CY")
+    # plt.show()
+
+    VLA_mean = frame["VLA"].mean()
+    SLA_mean = frame["SLA"].mean()
+    FWK_mean = frame["FWK"].mean()
+    OR_mean = frame["OR"].mean()
+    CY_mean = frame["CY"].mean()
+
+    methods = ["VLA","SLA","FWK","OR","CY"]
+    interesting_cols = ["edges","nodes","buds"]
+    # sns.boxplot(x=["VLA","SLA","FWK","OR","CY"], y=[VLA_mean,SLA_mean,FWK_mean,OR_mean,CY_mean])
+    runtimes_frame = frame[["VLA","SLA","FWK","OR","CY","edges","nodes","buds"]]
+    # runtimes_frame = frame[["VLA","FWK","OR","CY","edges","nodes","buds"]]
+    runtimes_frame = pd.melt(runtimes_frame, var_name="method", value_name="duration (ms)", id_vars=["edges","nodes","buds"])
+    runtimes_frame = runtimes_frame[runtimes_frame["duration (ms)"]>0]
+    # gfg = sns.boxplot(data=runtimes_frame, x="method", y="duration (ms)", orient="v", showfliers=False)
+    # plt.show()
+
+    overhead_frame = frame.copy()[methods+interesting_cols].drop(columns=["CY"])
+    overhead_perc_frame = frame.copy()[methods+interesting_cols].drop(columns=["CY"])
+    for m in methods:
+        if m=="CY":
+            continue
+        overhead_frame[m] = frame[m] - frame["CY"]
+        # overhead_perc_frame[m] = (overhead_frame[m]+frame["CY"])/frame["CY"]
+        overhead_perc_frame[m] = 100*overhead_frame[m]/frame["CY"]
+
+    overhead_frame = pd.melt(overhead_frame, var_name="method", value_name="overhead (ms)", id_vars=["edges","nodes","buds"])
+    overhead_perc_frame = pd.melt(overhead_perc_frame, var_name="method", value_name="overhead %", id_vars=["edges","nodes","buds"])
+
+    palette =sns.color_palette( ["#233D4D","#19C444", "#FCCA46", "#A1C181","#619B8A"])
+
+    sns.lineplot(data=runtimes_frame, x="edges", y="duration (ms)", hue="method", style="method", palette=palette, errorbar=("pi",50))
+    plt.yscale("log")
+    plt.show()
+
+    sns.lineplot(data=overhead_frame, x="edges", y="overhead (ms)", hue="method", style="method", palette=palette, errorbar=("pi",50))
+    plt.yscale("log")
+    # ax2= plt.twinx()
+    # sns.lineplot(data=frame, x="edges", y="CY", ax=ax2)
+    # ax2.sdt_ylabel("Cyclone runtime (ms)")
+    # plt.yscale("log")
+    plt.show()
+
+    gfg = sns.lineplot(data=overhead_perc_frame, x="edges", y="overhead %", hue="method", style="method", palette=palette, errorbar=("pi",50))
+    plt.yscale("log")
+    plt.show()
+
+    # gfg = sns.displot(data=runtimes_frame, x="duration", hue="method")
+    # gfg.set_ylim(-10,300)
+    # plt.show()
+    
+VLA_times = get_method_runtimes("./logs/results.VLA", 3, "VLA")
+SLA_times = get_method_runtimes("./logs/results.SLA", 3, "SLA")
+FWK_times = get_method_runtimes("./logs/results.FWK", 5, "FWK")
+OR_times = get_method_runtimes("./logs/results.OR", 5, "OR")
+SH_times = get_method_runtimes("./logs/results.no_TM", 7, "CY")
+SH_with_TM_times = get_method_runtimes("./logs/results.TM", 3, "CY with TM")
+
+stats_frame = get_graphs_stats("./stats.csv")
+
+
+frame = pd.concat([stats_frame, VLA_times, SLA_times, FWK_times, OR_times, SH_times, SH_with_TM_times], axis=1)
+frame = frame[frame["VLA"]>0]
+
+# print(sum(frame["VLA"]))
+# print(sum(frame["SLA"]))
+# print(sum(frame["FWK"]))
+# print(sum(frame["OR"]))
+# print(sum(frame["CY"]))
+# print(sum(frame["CY with TM"]))
+
+# compare_SH_with_and_without_TM(frame)
+plot_CY_performance(frame)
