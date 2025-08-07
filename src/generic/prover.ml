97bdb475e88276ac483e07551749e0c52e125eb3
Reuben Rowe <reuben.rowe@rhul.ac.uk>
2025-08-07 19:46:41 +0100

    Off by one "error": only an error insofar as the reported search depth is off by one.

--- src/generic/prover.ml
+++ src/generic/prover.ml
@@ -30,7 +30,7 @@ module Make (Seq : Sequent.S) (Infrule : Infrule.S) = struct
     if Int.( > ) bound maxbound then None
     else
       let rec dfs bound idx prf =
-        if Int.( < ) bound 0 then
+        if Int.( <= ) bound 0 then
           None
         else
           let () =
